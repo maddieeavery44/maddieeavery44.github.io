153104345b778c47942724bd82a3efbe302fcf5e
maddieeavery44 <82195893+maddieeavery44@users.noreply.github.com>
2021-04-09 00:40:12 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([38, -95], 4);
 var basemapUrl = 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}';
-var basemap = L.tileLayer(basemapURL).addTo(map);
+var basemap = L.tileLayer(https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}).addTo(map);
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
   layers: 'nexrad-n0r-900913',
